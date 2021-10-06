b3209c59322e9f907f11ab15c9346f4432653635
Bevard <cbevard1@gmail.com>
2021-10-06 14:35:13 -0400

    added S3LogCloser implementation that can be used by setting the property manager.wal.closer.implementation

---
+++ src/main/java/org/apache/accumulo/s3/manager/S3LogCloser.java
@@ -0,0 +1,16 @@
+package org.apache.accumulo.s3.manager;
+
+import org.apache.accumulo.core.conf.AccumuloConfiguration;
+import org.apache.accumulo.server.fs.VolumeManager;
+import org.apache.accumulo.server.manager.recovery.LogCloser;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+
+import java.io.IOException;
+
+public class S3LogCloser implements LogCloser {
+  @Override
+  public long close(AccumuloConfiguration accumuloConfiguration, Configuration configuration, VolumeManager volumeManager, Path path) throws IOException {
+    return 0;
+  }
+}
